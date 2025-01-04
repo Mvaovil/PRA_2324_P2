036cfd07454bec3d42bc340bfc32267ad9287976
Marc Vañó Villanueva <mvaovil@edu.upv.es>
2025-01-04 20:36:14 +0100

    Divide finalizado

---
+++ DyV.h
@@ -0,0 +1,18 @@
+#include <vector>
+#include <iostream>
+template <typename T>
+
+int BusquedaBinaria(T X, std::vector<T>& V, int ini, int fin){
+	if(ini > fin){
+		std::cout << "No se encuentra el array" << std::endl;
+		return -1;
+	}
+	int medio = (ini + fin) / 2;
+	if(V[medio] == X){
+		return medio;
+	}else if(V[medio] > X){
+		return BusquedaBinaria(X, V, ini, medio - 1);
+	}else{
+		return BusquedaBinaria(X, V, medio + 1, fin);
+	}
+}
